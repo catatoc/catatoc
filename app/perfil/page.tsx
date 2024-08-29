c0217bb49886bb05971cf6b51b1197203bcdb7e0
Carlos Horacio Carrasquero <ccarrasquero@correo.unimet.edu.ve>
2024-08-29 09:29:57 -0400

    feat: :sparkles: bubble video and profile

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -1,24 +1,35 @@
 "use client"
 
-import { Metadata } from "next"
+import { useEffect, useRef, useState } from "react"
 import Image from "next/image"
-import { MedalIcon } from "lucide-react"
 
 import { Carousel } from "@/components/ui/carousel"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
 import { AlbumArtwork } from "@/components/album-artwork"
+import EmojiConfetti from "@/components/triggerConfetti"
 
-// Datos de ejemplo
+// Datos de logros
 const achievements = [
   {
     name: "Logro 1",
-    image: "/images/logro1.png",
-    description: "Descripción breve del logro 1.",
+    cover: "/images/achievements/achievement1.png",
+    artist: "Descripción 1.",
   },
   {
     name: "Logro 2",
-    image: "/images/logro2.png",
-    description: "Descripción breve del logro 2.",
+    cover: "/images/achievements/achievement2.png",
+    artist: "Descripción 2.",
+  },
+  {
+    name: "Logro 3",
+    cover: "/images/achievements/achievement3.png",
+    artist: "Descripción 3.",
+  },
+  {
+    name: "Logro 4",
+    cover: "/images/achievements/achievement4.png",
+    artist: "Descripción 4.",
   },
 ]
 
@@ -28,24 +39,52 @@ const familyPhotos = [
   // Añade más fotos según sea necesario
 ]
 
-// export const metadata: Metadata = {
-//   title: "Perfil de [Tu Nombre]",
-//   description: "Sección de perfil de [Tu Nombre].",
-// }
+const emoji = "⚽" // Usar la pelota de fútbol como emoji
 
 export default function ProfilePage() {
+  const [selectedAchievement, setSelectedAchievement] = useState<any>(null)
+  const [showVideoBubble, setShowVideoBubble] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  // Mostrar la burbuja del video después de 5 segundos
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowVideoBubble(true)
+      // Intentar reproducir el video automáticamente con sonido
+      if (videoRef.current) {
+        videoRef.current.muted = false
+        videoRef.current.play().catch((error) => {
+          console.error("Error al reproducir el video automáticamente:", error)
+        })
+      }
+    }, 5000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  // Manejar el clic en la burbuja de video para activar el sonido si está silenciado
+  const handleVideoBubbleClick = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = false // Activar el sonido
+      videoRef.current.play().catch((error) => {
+        console.error("Error al intentar reproducir el video:", error)
+      })
+    }
+  }
+
   return (
     <div className="px-4 py-6 lg:px-8">
       {/* Avatar y Nombre */}
-      <div className="text-center mb-8">
+      <div className="mb-8 text-center">
         <Image
-          src="/images/tu-foto.png"
+          src="/profile.png"
           width={150}
           height={150}
           alt="[Tu Nombre]"
-          className="rounded-full mx-auto"
+          className="mx-auto rounded-full"
         />
-        <h1 className="text-2xl font-semibold mt-4">[Tu Nombre]</h1>
+        <h1 className="mt-4 text-2xl font-semibold">
+          Carlos Horacio Carrasquero
+        </h1>
       </div>
 
       {/* Sección: Mis Logros */}
@@ -53,20 +92,32 @@ export default function ProfilePage() {
         <h2 className="text-2xl font-semibold tracking-tight">Mis Logros</h2>
         <Separator className="my-4" />
         <div className="relative">
-          <div className="flex space-x-4 pb-4">
-            {achievements.map((achievement) => (
-              <div className="relative" key={achievement.name}>
-                {/* <AlbumArtwork
-                  album={achievement}
-                  className="w-[150px] shrink-0"
-                  aspectRatio="square"
-                  width={150}
-                  height={150}
-                /> */}
-                <MedalIcon className="absolute top-0 right-0 h-6 w-6 text-gold" />
-              </div>
-            ))}
-          </div>
+          <ScrollArea className="flex w-96 gap-4 whitespace-nowrap rounded-md border md:w-full">
+            <div className="flex space-x-8">
+              {achievements.map((achievement) => (
+                <div
+                  className="relative cursor-pointer"
+                  key={achievement.name}
+                  onClick={() => setSelectedAchievement(achievement)}
+                >
+                  <EmojiConfetti
+                    key={achievement.name}
+                    emoji={emoji} // Aquí puedes usar "⚽" o cualquier emoji que desees
+                  >
+                    <AlbumArtwork
+                      album={achievement}
+                      className="w-[150px] shrink-0"
+                      aspectRatio="square"
+                      width={150}
+                      height={150}
+                    />
+                  </EmojiConfetti>
+                  <span className="absolute right-0 top-0 text-2xl">🏅</span>
+                </div>
+              ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
         </div>
       </div>
 
@@ -74,13 +125,13 @@ export default function ProfilePage() {
       <div className="mb-8">
         <h2 className="text-2xl font-semibold tracking-tight">Familia</h2>
         <Separator className="my-4" />
-        <Carousel className="w-full">
+        <Carousel className="hidden w-full">
           {familyPhotos.map((photo, index) => (
             <Image
               key={index}
               src={photo}
-              width={300}
-              height={200}
+              width={150}
+              height={150}
               alt={`Foto de Familia ${index + 1}`}
               className="rounded-md"
             />
@@ -119,6 +170,47 @@ export default function ProfilePage() {
           cómo eso impacta en tu vida y en los demás.
         </p>
       </div>
+
+      {/* Modal para Logro Seleccionado */}
+      {selectedAchievement && (
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 p-8"
+          onClick={() => setSelectedAchievement(null)}
+        >
+          <div className="relative">
+            <Image
+              src={selectedAchievement.cover}
+              alt={selectedAchievement.name}
+              width={800}
+              height={800}
+              className="rounded-md"
+              objectFit="contain"
+            />
+            <p className="mt-4 text-center text-white">
+              {selectedAchievement.artist}
+            </p>
+          </div>
+        </div>
+      )}
+
+      {/* Burbuja de Video */}
+      {showVideoBubble && (
+        <div className="fixed bottom-4 left-4 z-50">
+          <div
+            className="relative size-24  cursor-pointer overflow-hidden rounded-full shadow-lg"
+            onClick={handleVideoBubbleClick}
+          >
+            <video
+              ref={videoRef}
+              src="/video/video.mp4"
+              className="size-full object-cover"
+              autoPlay
+              preload="auto"
+              controls
+            />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
